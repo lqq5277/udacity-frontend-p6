7ed9b8502c6bc9d09d13bb970b2142c00422af31
Ivy Liang <lqqusc@gmail.com>
2016-04-03 11:58:52 -0700

    Step 1~5

--- js/app.js
+++ js/app.js
@@ -1,40 +1,55 @@
-var favoriteLocations = [{
+var favoritePlaces = [{
   name: "Westfield Valley Fair",
-  position: {lat: 37.325739, lng: -121.945916}
+  position: {lat: 37.325739, lng: -121.945916},
+  address: "2855 Stevens Creek Blvd, Santa Clara, CA 95050"
 }, {
-  name: "City Sports",
-  position: {lat: 37.349231, lng: -121.921128}
+  name: "City Sports Club",
+  position: {lat: 37.349231, lng: -121.921128},
+  address:"610 Newhall Dr, San Jose, CA 95110"
 }, {
-  name: "Library",
-  position: {lat: 37.335436, lng: -121.884975}
+  name: "City Sports Club",
+  position: {lat: 37.383839, lng: -121.898053},
+  address:"1045 E Brokaw Rd, San Jose, CA 95131"
 }, {
-  name: "Golf",
-  position: {lat: 37.378352, lng: -121.891850}
+  name: "Dr. Martin Luther King, Jr. Library",
+  position: {lat: 37.335436, lng: -121.884975},
+  address: "150 E San Fernando St, San Jose, CA 95112"
 }, {
-  name: "Matrix Casion",
-  position: {lat: 37.371216, lng: -121.919590}
+  name: "Northside Branch Library",
+  position: {lat: 37.395717, lng: -121.946975},
+  address: "695 Moreland Way, Santa Clara, CA 95054"
+}, {
+  name: "San Jose Municipal Golf Course",
+  position: {lat: 37.378352, lng: -121.891850},
+  address: "1560 Oakland Rd, San Jose, CA 95131"
+}, {
+  name: "Santa Clara Golf & Tennis Club",
+  position: {lat: 37.406211, lng: -121.971746},
+  address: "5155 Stars and Stripes Dr, Santa Clara, CA 95054"
+}, {
+  name: "99 Ranch Market",
+  position: {lat: 37.422845, lng: -121.916828},
+  address: "338 Barber Ln, Milpitas, CA 95035"
+}, {
+  name: "99 Ranch Market",
+  position: {lat: 37.386385, lng: -121.884403},
+  address: "1688 Hostetter Rd, San Jose, CA 95131"
 }];
 
 var map;
 var markers = [];
 function initMap() {
-  var homeLatLng = {lat: 37.393283, lng: -121.903051};
+  var centerLatLng = {lat: 37.393283, lng: -121.903051};
   map = new google.maps.Map(document.getElementById('map'), {
-    center: homeLatLng,
+    center: centerLatLng,
     zoom: 12
   });
 
-  var marker = new google.maps.Marker({
-    position: homeLatLng,
-    map: map,
-    title: 'Sweet Home!'
-  });
-
-  for (var location in favoriteLocations) {
+  for (var place in favoritePlaces) {
     markers.push(new google.maps.Marker({
-      position: favoriteLocations[location].position,
+      position: favoritePlaces[place].position,
       map: map,
-      title: favoriteLocations[location].name
+      title: favoritePlaces[place].name
     }));
   }
   setMapOnAll(map);
@@ -46,28 +61,30 @@ function setMapOnAll(map) {
   }
 }
 
-var Location = function(data) {
+var Place = function(data) {
   this.name = ko.observable(data.name);
   this.position = ko.observable(data.position);
+  this.address = ko.observable(data.address);
 }
 
 var ViewModel = function() {
   var self = this;
-  this.locations = ko.observableArray([]);
-  favoriteLocations.forEach(function(locationItem) {
-    self.locations.push(new Location(locationItem));
+  this.showingPlaces = ko.observableArray([]);
+  favoritePlaces.forEach(function(placeItem) {
+    self.showingPlaces.push(new Place(placeItem));
   });
 
-  this.showInfo = function(clickedLocation) {
+  this.filterString = ko.observable('');
 
-  }
+  this.showInfo = function(clickedPlace) {
+
+  };
 
   this.filterPlaces = function() {
 
-  }
+  };
 }
 
 window.addEventListener('load', initMap);
 
 ko.applyBindings(new ViewModel());
-
